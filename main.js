a5d10dffd736a9443da7fc4a52f34d429b20e169
Abimael Santos <juanabimael@gmail.com>
2020-04-19 20:02:30 -0500

    removal of unwanted code

--- main.js
+++ main.js
@@ -3,5 +3,3 @@ function changeTitle(newOne) {
 }
 
 changeTitle('Quantum Mechanics');
-
-let Professions = ['Doctor', 'Artist', 'Astronaut', 'Chef', 'Construction Worker', 'Firefighter', 'Police', 'Teacher', 'Veterinarian', 'Actress/Actor', 'Architect', 'Singer', 'Dentist', 'Detective', 'Writer', 'Farmer', 'Nurse', 'Pilot', 'Engineer', 'Accountant', 'Butcher', 'Cashier', 'Barber', 'Carpenter', 'Baker', 'Electritian', 'Flight Attendant', 'Plumber', 'Photographer', 'Receptionist', 'Scientist', 'Lawyer', 'Bus Driver', 'Designer', 'Journalist', 'Lifeguard', 'Musician', 'Painter', 'Florist', 'Sales Assistant', 'Mechanic', 'Model', 'Shop Assistant', 'Politician', 'Translator', 'Hairdresser', 'Taxi Driver', 'Pharmacist', 'Travel Agent', 'Cleaner', 'Biologist', 'Dancer', 'Gardener', 'Meteorologist', 'Postman', 'Programmer', 'Nanny', 'Travel Guide', 'Salesman', 'Researcher'];
